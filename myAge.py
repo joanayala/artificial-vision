192b4b18160e925b884b84bca3883672d4702704
joanayaladev <joancayala@gmail.com>
2020-08-27 12:13:39 -0500

    Upload myAge script

---
+++ myAge.py
@@ -0,0 +1,13 @@
+#Developer: Joan C. Ayala
+'''
+What does this script do?:
+This script show your name and your age.
+'''
+
+myName = 'Joan C. Ayala'
+myDate = 1985
+year = 2020
+
+age = year - myDate
+
+print ("Your name is: ", myName, "and you are ", age, "years old")
