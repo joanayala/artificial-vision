83856500c0d1a8075d344b0f570a60368ee6871f
joanayala <jflaco85@gmail.com>
2020-09-01 12:10:21 -0500

    Basic Calcs v1, v2, v3

---
+++ basic_calc1.py
@@ -0,0 +1,13 @@
+#Developer: Joan C. Ayala
+#Script: this program let you add two static numbers
+
+#clear screen class
+import os
+
+os.system("cls")
+
+x = 20
+y = 30
+
+add = x + y
+print("The add is: ", add)
--- myAge.py
+++ myAge.py
@@ -4,6 +4,19 @@ What does this script do?:
 This script show your name and your age.
 '''
 
+'''
+How config git in my computer?
+1st. git config --global user.name "myusername"
+2nd. git config --global user.email "mygitemail"
+
+
+How make a tracking/staging to GitHub or GitLab
+
+1st. git add . / git add --all / git add file_name
+2nd. git commit -m "Your message"
+3rd. git push origin master
+'''
+
 myName = 'Joan C. Ayala'
 myDate = 1985
 year = 2020
